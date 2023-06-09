62031b932f04b2f34a8f33c44aa136138e39dd74
mgroth0 <mgroth49@gmail.com>
2023-06-08 21:30:18 -0400

    changed src/main/kotlin/matt/rtask/profile/profile.kt

--- src/main/kotlin/matt/rtask/profile/profile.kt
+++ src/main/kotlin/matt/rtask/profile/profile.kt
@@ -1,15 +1,15 @@
 package matt.rtask.profile
 
 import matt.file.context.ComputeContext
-import matt.lang.IsProfiling
 import matt.lang.function.Op
+import matt.lang.profiling.IsProfilingWithJProfiler
 import matt.log.profile.jp.JProfiler
 import matt.log.profile.real.Profiler
 
 context(ComputeContext)
 inline fun openMindProfile(op: Op) {
     val profiler = Profiler(
-        enableAll = IsProfiling,
+        enableAll = IsProfilingWithJProfiler,
         engine = JProfiler(snapshotFolder = files.snapshotFolder)
     ) {
         println("saved snapshot: $it")
